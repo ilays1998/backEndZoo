0ffb52f97ca3ae43a39f24ecc98862994cc0103f
ilay <ilaysoffer@gmail.com>
2023-09-18 08:25:34 +0300

    after appending DB

--- animal_schema.py
+++ animal_schema.py
@@ -2,17 +2,7 @@ from __future__ import annotations
 import strawberry
 import typing
 from typing import List
-AnimalMetadata(
 
-)
-DB = {
-    "animals": {
-        [
-            Animal()
-        ]
-    },
-    "metadata"
-}
 @strawberry.interface
 class Node:
     id: strawberry.ID
@@ -31,21 +21,9 @@ class AnimalMetadata(Node):
 
 
 url = strawberry.scalar(
-        typing.NewType("url", str),
-        parse_value=lambda v: str(v)
+    typing.NewType("url", str),
+    parse_value=lambda v: str(v)
 )
-#
-# type Animal implements Node {
-#     name: String!
-#     image: URL!
-#     age: Int!
-#     metadata: AnimalMetadata!
-# }
-
-
-
-
-
 
 @strawberry.type
 class Animal(Node):
@@ -61,30 +39,70 @@ class Animal(Node):
                                   _class="Reptilia",
                                   superfamily="Squamata",
                                   family="Serpentes")
+    @strawberry.
 
     @classmethod
     def from_dict(cls, data: dict) -> Animal:
         return Animal(**data)
 
+
+
+DB = {
+    "animals": {
+        [
+            Animal(
+                name="snake",
+                image="https://sensorytools.net/cdn/shop/products/1_98351ebe-0205-4a5c-b121-f987fa950fce.png?v=1681262929",
+                age=5
+            ),
+            Animal(
+                name="lion",
+                image="https://th-thumbnailer.cdn-si-edu.com/7-edQjVy53tlGD2OhGdl-mrY1K8=/fit-in/1600x0/https%3A%2F%2Ftf-cmsv2-smithsonianmag-media.s3.amazonaws.com%2Ffiler%2Fa9%2Fff%2Fa9ff31d0-aecd-464e-80c7-873e4651cd2b%2Fmufasa.jpeg",
+                age=23
+            )
+        ]
+    },
+    "metadata": {
+        [
+            AnimalMetadata(domain="Eukaryota",
+                           kingdom="Animalia",
+                           pyhlum="Chordata",
+                           _class="Reptilia",
+                           superfamily="Squamata",
+                           family="Serpentes"),
+            AnimalMetadata(domain="Eukaryota",
+                           kingdom="Animalia",
+                           pyhlum="Chordata",
+                           _class="Mammalia",
+                           superfamily="Felidae",
+                           family="Pantherinae")
+        ]
+    }
+}
+
+#
+# type Animal implements Node {
+#     name: String!
+#     image: URL!
+#     age: Int!
+#     metadata: AnimalMetadata!
+# }
+
+
+
+
+
+
+
 def get_animals() -> List[Animal]:
-    return [Animal(
-        name="snake",
-        image="https://sensorytools.net/cdn/shop/products/1_98351ebe-0205-4a5c-b121-f987fa950fce.png?v=1681262929",
-        age=5
-    ),
-        Animal(
-            name="snake",
-            image="https://sensorytools.net/cdn/shop/products/1_98351ebe-0205-4a5c-b121-f987fa950fce.png?v=1681262929",
-            age=23
-        )]
+    return []
 
 
 @strawberry.type
 class Query:
     animals: List[Animal] = strawberry.field(resolver=get_animals)
 
-
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, types=[Node, Animal])
 
 
 
