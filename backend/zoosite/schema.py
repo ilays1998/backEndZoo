3185f2298dc088427c8625267b0880062e6e88a1
ilays <ilaysoffer@gmail.com>
2023-09-20 09:09:17 +0300

    remove unnecessary comments

--- backend/zoosite/schema.py
+++ backend/zoosite/schema.py
@@ -58,53 +58,6 @@ class Animal(Node):
         return animal
 
 
-# DB = {
-#     "animals": [
-#         {
-#             "id": "1",
-#             "name": "snake",
-#             "image": "https://sensorytools.net/cdn/shop/products/1_98351ebe-0205-4a5c-b121-f987fa950fce.png?v=1681262929",
-#             "age": 5,
-#         },
-#         {
-#             "id": "2",
-#             "name": "lion",
-#             "image": "https://th-thumbnailer.cdn-si-edu.com/7-edQjVy53tlGD2OhGdl-mrY1K8=/fit-in/1600x0/https%3A%2F%2Ftf-cmsv2-smithsonianmag-media.s3.amazonaws.com%2Ffiler%2Fa9%2Fff%2Fa9ff31d0-aecd-464e-80c7-873e4651cd2b%2Fmufasa.jpeg",
-#             "age": 23,
-#         },
-#     ],
-#     "metadata": [
-#         {
-#             "id": "2",
-#             "domain": "Eukaryota",
-#             "kingdom": "Animalia",
-#             "pyhlum": "Chordata",
-#             "_class": "Mammalia",
-#             "superfamily": "Felidae",
-#             "family": "Pantherinae",
-#         },
-#         {
-#             "id": "1",
-#             "domain": "Eukaryota",
-#             "kingdom": "Animalia",
-#             "pyhlum": "Chordata",
-#             "_class": "Reptilia",
-#             "superfamily": "Squamata",
-#             "family": "Serpentes",
-#         }
-#     ],
-# }
-
-
-#
-# type Animal implements Node {
-#     name: String!
-#     image: URL!
-#     age: Int!
-#     metadata: AnimalMetadata!
-# }
-
-
 def get_animals() -> list[Animal]:
     animals = models.Animal.objects.all()
     return [Animal.from_orm(obj) for obj in animals]
